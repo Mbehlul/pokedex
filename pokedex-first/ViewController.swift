3833071c5f3353eb7b4e1e02e8ceacd759df8895
Mbehlul <behlulkusaslan@gmail.com>
2016-01-23 15:58:24 +0200

    Initial Commit

---
+++ pokedex-first/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-first
+//
+//  Created by Behlül Kuşaslan on 23/01/16.
+//  Copyright © 2016 Behlül Kuşaslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
